5caef3e09ac7a357c3cf1956fd264f361cba00d5
Abdul Rahaman <Abdul Rahaman@DESKTOP-7L157N1>
2020-06-12 09:21:30 +0530

    Command Line arguments problems

---
+++ Milestone1/src/com/wipro/languagebasics/Problem1.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+public class Problem1 
+{
+	public static void main(String args[])
+	{
+		String a=args[0];
+		String b=args[1];
+		String c="Technologies";
+		System.out.println(a+" "+c+" "+b);
+	}
+
+}
---
+++ Milestone1/src/com/wipro/languagebasics/Problem2.java
@@ -0,0 +1,11 @@
+package com.wipro.languagebasics;
+public class Problem2 
+{
+    public static void main(String args[])
+    {
+			String a=args[0];
+			String c="Welcome ";
+			System.out.println(c+""+a);
+		}
+
+}
