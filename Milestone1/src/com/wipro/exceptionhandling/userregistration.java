b66dc108c88c1831c2d7c20608a0c6e81477f5a1
AbdulRahaman-123 <60355004+AbdulRahaman-123@users.noreply.github.com>
2020-07-27 11:46:20 +0530

    Add files via upload

---
+++ Milestone1/src/com/wipro/exceptionhandling/userregistration.java
@@ -0,0 +1,29 @@
+package com.wipro.exceptionhandling;
+class userregistration {
+	
+	public void registerUser(String username, String userCountry) throws InvalidCountryException {
+		if (!userCountry.equals("India"))
+			throw new InvalidCountryException();
+		else
+			System.out.println("User registration done successfully");
+		
+		
+	}
+
+	public static void main(String[] args) {
+		userregistration registration = new userregistration();
+		
+		try {
+			registration.registerUser("Rohith", "India");
+		} catch (InvalidCountryException e) {
+		}
+	}
+
+}
+class InvalidCountryException extends Exception {
+	public InvalidCountryException() {
+		super();
+		System.out.println("InvalidCountryException occured");
+		System.out.println("User Outside India  cannot be registered");
+	}
+}
